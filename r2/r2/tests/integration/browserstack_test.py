92077239bd414dd6d01d38cf06fafa3a51daf950
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-18 16:22:21 -0400

    Adds browserstack integraton test

---
+++ r2/r2/tests/integration/browserstack_test.py
@@ -0,0 +1,29 @@
+import os
+from r2.tests import *
+
+# Selenium installation:
+# pip install -U selenium
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class BrowserStackTest(RedditTestCase):
+    def test_set_up(self):
+        caps = {}
+        caps["browser"] = "IE"
+        caps["browser_version"] = "7.0"
+        caps["os"] = "Windows"
+        caps["os_version"] = "XP"
+        caps["browserstack.tunnel"] = "true"
+        caps["browserstack.debug"] = "true"
+        bs_username = str(os.environ.get('BROWSERSTACKUSERNAME'))
+        bs_key = str(os.environ.get('BROWSERSTACKKEY'))
+        bs_url = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
+        self.driver = webdriver.Remote( command_executor=bs_url, desired_capabilities=caps)
+        
+    def test_browser_stack(self):
+        driver = self.driver
+        driver.get("http://selenium-target.lightnetb.org")
+        self.assert_equal('recently active comments : lightnet', driver.title)
+        print driver.title
+        driver.quit()
+        self.driver.quit()
