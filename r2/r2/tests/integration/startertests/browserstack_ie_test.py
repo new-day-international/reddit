5b2b97c88b4757a8fc65c2e050c85a499d511bf1
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-21 15:16:38 -0400

    Removes the offending test until further notice

--- r2/r2/tests/integration/startertests/browserstack_ie_test.py
+++ r2/r2/tests/integration/startertests/browserstack_ie_test.py
@@ -31,21 +31,6 @@ class BrowserStackInternetExplorerTest(RedditTestCase):
         driver.get(self.base_url)
         self.assert_equal('recently active comments : lightnet', driver.title)
 
-    def test_register_user(self):
-        driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_link_text("register").click()
-        driver.find_element_by_id("user_reg").send_keys("Test User")
-        driver.find_element_by_id("email_reg").send_keys("tester@lightnetb.org")
-        driver.find_element_by_id("passwd_reg").send_keys("password")
-        driver.find_element_by_id("passwd2_reg").send_keys("password")
-        driver.find_element_by_id("passwd2_reg").submit()
-        try:
-            WebDriverWait(driver, 30).until(EC.presence_of_element_located(By.CLASS_NAME, "userkarma"))
-        finally:
-            username = driver.find_element_by_css_selector("#header .user a").text
-        self.assert_equal("Test_User_1", username)
-                
     def tearDown(self):
         self.driver.quit()
 
