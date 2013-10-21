89c65f23111a3d0f1ab2a811f95e5125ee0a4bc1
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-21 09:39:00 -0400

    Adds appropriate setUp and tearDown

--- r2/r2/tests/integration/browserstack_test.py
+++ r2/r2/tests/integration/browserstack_test.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class BrowserStackTest(RedditTestCase):
-    def test_set_up(self):
+    def setUp(self):
         caps = {}
         caps["browser"] = "IE"
         caps["browser_version"] = "7.0"
@@ -19,11 +19,15 @@ class BrowserStackTest(RedditTestCase):
         bs_key = str(os.environ.get('BROWSERSTACKKEY'))
         bs_url = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
         self.driver = webdriver.Remote( command_executor=bs_url, desired_capabilities=caps)
-        
+
     def test_browser_stack(self):
         driver = self.driver
         driver.get("http://selenium-target.lightnetb.org")
         self.assert_equal('recently active comments : lightnet', driver.title)
-        print driver.title
-        driver.quit()
+
+    def tearDown(self):
         self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
