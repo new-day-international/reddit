35c02db0414a36858b9a40d6a0ea75d861fb672a
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-21 10:57:12 -0400

    Add firefox test

--- r2/r2/tests/integration/browserstack_test.py
+++ r2/r2/tests/integration/browserstack_test.py
@@ -6,7 +6,7 @@ from r2.tests import *
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class BrowserStackTest(RedditTestCase):
+class BrowserStackInternetExplorerTest(RedditTestCase):
     def setUp(self):
         caps = {}
         caps["browser"] = "IE"
@@ -28,6 +28,27 @@ class BrowserStackTest(RedditTestCase):
     def tearDown(self):
         self.driver.quit()
 
+class BrowserStackFirefoxTest(RedditTestCase):
+    def setUp(self):
+        caps = {}
+        caps["browser"] = "Firefox"
+        caps["browser_version"] = "24.0"
+        caps["os"] = "Windows"
+        caps["os_version"] = "8"
+        caps["browserstack.tunnel"] = "true"
+        caps["browserstack.debug"] = "true"
+        bs_username = str(os.environ.get('BROWSERSTACKUSERNAME'))
+        bs_key = str(os.environ.get('BROWSERSTACKKEY'))
+        bs_url = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
+        self.driver = webdriver.Remote( command_executor=bs_url, desired_capabilities=caps)
+
+    def test_browser_stack(self):
+        driver = self.driver
+        driver.get(str(os.environ.get('BROWSERSTACKURL')))
+        self.assert_equal('recently active comments : lightnet', driver.title)
+
+    def tearDown(self):
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
