6acf0130f7ff33e32ab92ee1b0b4723f67855a84
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-21 11:08:37 -0400

    Separates firefox from ie tests

--- r2/r2/tests/integration/startertests/browserstack_ff_test.py
+++ r2/r2/tests/integration/startertests/browserstack_ff_test.py
@@ -6,28 +6,6 @@ from r2.tests import *
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class BrowserStackInternetExplorerTest(RedditTestCase):
-    def setUp(self):
-        caps = {}
-        caps["browser"] = "IE"
-        caps["browser_version"] = "7.0"
-        caps["os"] = "Windows"
-        caps["os_version"] = "XP"
-        caps["browserstack.tunnel"] = "true"
-        caps["browserstack.debug"] = "true"
-        bs_username = str(os.environ.get('BROWSERSTACKUSERNAME'))
-        bs_key = str(os.environ.get('BROWSERSTACKKEY'))
-        bs_url = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
-        self.driver = webdriver.Remote( command_executor=bs_url, desired_capabilities=caps)
-
-    def test_browser_stack(self):
-        driver = self.driver
-        driver.get(str(os.environ.get('BROWSERSTACKURL')))
-        self.assert_equal('recently active comments : lightnet', driver.title)
-
-    def tearDown(self):
-        self.driver.quit()
-
 class BrowserStackFirefoxTest(RedditTestCase):
     def setUp(self):
         caps = {}
