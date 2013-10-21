78c3621f67aa41cb23e6f7c509d15c921102d7b0
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-10-21 11:50:11 -0400

    Adds login test

--- r2/r2/tests/integration/startertests/browserstack_ie_test.py
+++ r2/r2/tests/integration/startertests/browserstack_ie_test.py
@@ -7,6 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class BrowserStackInternetExplorerTest(RedditTestCase):
+    bs_username = str(os.environ.get('BROWSERSTACKUSERNAME'))
+    bs_key = str(os.environ.get('BROWSERSTACKKEY'))
+    bs_api = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
+
+    url = str(os.environ.get('BROWSERSTACKURL'))
+
     def setUp(self):
         caps = {}
         caps["browser"] = "IE"
@@ -15,15 +21,23 @@ class BrowserStackInternetExplorerTest(RedditTestCase):
         caps["os_version"] = "XP"
         caps["browserstack.tunnel"] = "true"
         caps["browserstack.debug"] = "true"
-        bs_username = str(os.environ.get('BROWSERSTACKUSERNAME'))
-        bs_key = str(os.environ.get('BROWSERSTACKKEY'))
-        bs_url = "http://" + bs_username + ":" + bs_key + "@hub.browserstack.com/wd/hub"
-        self.driver = webdriver.Remote( command_executor=bs_url, desired_capabilities=caps)
+        self.driver = webdriver.Remote( command_executor=self.bs_api, desired_capabilities=caps)
 
     def test_browser_stack(self):
         driver = self.driver
-        driver.get(str(os.environ.get('BROWSERSTACKURL')))
+        driver.get(self.url)
         self.assert_equal('recently active comments : lightnet', driver.title)
+
+    def test_register_user(self):
+        driver = self.driver
+        driver.open(self.url)
+        driver.click("link=register")
+        driver.type("id=user_reg", "Test User")
+        driver.type("id=email_reg", "tester@lightnetb.org")
+        driver.type("id=passwd_reg", "password")
+        driver.type("id=passwd2_reg", "password")
+        driver.click("css=button.button")
+        self.assert_equal('Test_User_1', driver.find_element_by_css_selector("#header .user a"))
 
     def tearDown(self):
         self.driver.quit()
