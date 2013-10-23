992a68cba90a546ca9f5fffc7165e8d15e552093
Curtis Faith <curtis.m.faith@gmail.com>
2013-10-23 11:49:55 -0400

    Issue #155 - Houses UI Tweaks

--- r2/r2/models/subreddit.py
+++ r2/r2/models/subreddit.py
@@ -606,10 +606,21 @@ class Subreddit(Thing, Printable):
 
             house_rules = '#### [](#h4-green)\n> ## [House Rules](#icon-information)\n'
             if not item.space_is_house:
-                house_rules += '> (using house rules from **'
-                house_rules += item.use_rules_from_space if item.use_rules_from_space else ""
-                house_rules += '**)\n\n>&nbsp;\n\n>'
+                rules_from = item.use_rules_from_space if item.use_rules_from_space else ""
+                house_rules += ">House Rules from **[" + rules_from + "]"
+                house_rules += "(http://" + g.domain + "/space/" + rules_from + ")"
+                house_rules += "\:**\n\n>"
+                house_rules += "\n\n>"
             house_rules += item.get_house_rules().replace('\n','\n\n>')
+            house_rules += "\n\n>House Color for **"
+            rules_from = item.use_rules_from_space if item.use_rules_from_space else item.name
+            house_rules += "[" + rules_from  + "]"
+            house_rules += "(http://" + g.domain + "/space/" + rules_from + ")"
+            house_rules += "\:**"
+            house_rules += "\n\n><div class=\"house_color\" "
+            house_rules += "style=\"width:265px; background-color: "
+            house_rules += item.get_house_color()
+            house_rules += "; float: left; margin-left: 10px; margin-top: 4px; height: 20px\"></div>&nbsp;"
 
             item.house_rules_usertext = UserText(   item,
                                                     house_rules,
