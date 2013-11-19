dbf473d9e22e23994a504f1a007f359abed298cd
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-11-19 17:08:48 -0500

    Front page lists updated

--- r2/r2/public/static/js/scripts.js
+++ r2/r2/public/static/js/scripts.js
@@ -1,4 +1,7 @@
 // Run all jQuery
 $(function () {
+  $('body').find('img').each(function(){
+    $(this).attr('src', $(this).attr('src').replace('test.lightnet','lightnet'));
+  });
   // $(".dropdown-toggle").dropdown();
 });
