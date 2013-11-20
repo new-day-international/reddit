29580de26a2f0fd8d6e4d2afe0a55559615d755c
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-11-20 15:34:45 -0500

    Adds sidebar collapser

--- r2/r2/public/static/js/scripts.js
+++ r2/r2/public/static/js/scripts.js
@@ -3,5 +3,14 @@ $(function () {
   $('body').find('img').each(function(){
     $(this).attr('src', $(this).attr('src').replace('test.lightnet','lightnet'));
   });
+
+  $(".tall-collapse-button").click(function(){
+    var sidebar_width = $("#left-sidebar").outerWidth() - 1;
+
+    if($(this).parent().hasClass("col-md-3"))
+        $("#left-sidebar").addClass("col-md-0").removeClass("col-md-3").width("10");
+    else
+        $("#left-sidebar").removeClass("col-md-0").addClass("col-md-3").width("auto");
+  })
   // $(".dropdown-toggle").dropdown();
 });
