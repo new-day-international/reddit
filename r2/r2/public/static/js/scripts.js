afefd0bd00a2baed0b548bd51e0a46b712fa5faa
Myers Carpenter <myers@maski.org>
2013-11-20 00:37:59 -0500

    log out link works

--- r2/r2/public/static/js/scripts.js
+++ r2/r2/public/static/js/scripts.js
@@ -1,7 +1,17 @@
 // Run all jQuery
 $(function () {
-  $('body').find('img').each(function(){
-    $(this).attr('src', $(this).attr('src').replace('test.lightnet','lightnet'));
-  });
-  // $(".dropdown-toggle").dropdown();
+    $('body').find('img').each(function(){
+        $(this).attr('src', $(this).attr('src').replace('test.lightnet','lightnet'));
+    });
+    // $(".dropdown-toggle").dropdown();
+});
+
+
+$(document).on('click.postlink', '.postlink', function (e) {
+    e.preventDefault();
+    $('<form>', {
+        'action': $(e.target).data('action'),
+        'method': 'POST',
+        'html': '<input type="hidden" name="uh" value="' + Reddit.modhash + '" />'
+    }).appendTo(document.body).submit();
 });
